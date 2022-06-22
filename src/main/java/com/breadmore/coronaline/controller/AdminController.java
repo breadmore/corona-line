dc3db9140816d1cb9d15de1f7dc2ca18b3f8fa0f
BM <lepo3@naver.com>
2022-06-23 00:48:10 +0900

    Edit #2 - fix
    fix

--- src/main/java/com/breadmore/coronaline/controller/AdminController.java
+++ src/main/java/com/breadmore/coronaline/controller/AdminController.java
@@ -1,13 +1,7 @@
 package com.breadmore.coronaline.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequestMapping("/admin")
 @Controller
